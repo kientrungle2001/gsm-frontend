16463469bcbbe7c801be4e3fe37b32f9c4e95e46
Le Trung Kien <kienlt@smartosc.com>
2020-06-30 14:15:10 +0700

    test 3

--- src/app/code/Smart/Customer/Block/AccountGrid.js
+++ src/app/code/Smart/Customer/Block/AccountGrid.js
@@ -4,24 +4,26 @@ class SmartCustomerBlockAccountGrid extends React.Component {
 	render() {
 		let {rows, header} = this.props;
 		return <div className="mt-3">
-			<table className="table table-dark">
-				<thead>
-					<tr>
-						{header.map(function(col, index) {
-							return (<td key={"col-index-" + index}>{col.name}</td>);
-						})}
-					</tr>
-				</thead>
-				<tbody>
-						{rows.forEach(function(row, rowIndex) {
-							return (
-								<tr key={"row-index-" + rowIndex}>
+			<div className="row">
+				<table className="table table-dark col-md-3">
+					<thead>
+						<tr>
+							{header.map(function(col, index) {
+								return (<td key={"col-index-" + index}>{col.name}</td>);
+							})}
+						</tr>
+					</thead>
+					<tbody>
+							{rows.forEach(function(row, rowIndex) {
+								return (
+									<tr key={"row-index-" + rowIndex}>
 
-								</tr>
-							);
-						})}
-				</tbody>
-			</table>
+									</tr>
+								);
+							})}
+					</tbody>
+				</table>
+			</div>
 		</div>;
 	}
 }
