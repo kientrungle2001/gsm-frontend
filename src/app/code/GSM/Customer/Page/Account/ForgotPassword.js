a02484119ff65550cfe7ecee285569e3965430eb
Le Trung Kien <kienlt@smartosc.com>
2020-07-06 14:15:45 +0700

    redux install

--- src/app/code/GSM/Customer/Page/Account/ForgotPassword.js
+++ src/app/code/GSM/Customer/Page/Account/ForgotPassword.js
@@ -8,35 +8,37 @@ class GSMCustomerPageAccountForgotPassword extends React.Component {
 	render() {
 		return (
 			<>
-				<div className="container mt-5">
-					<div className="d-flex justify-content-center h-100">
-						<div className="card">
-							<div className="card-header">
-								<h3>Quên mật khẩu</h3>
-								<div className="d-flex justify-content-end social_icon">
-									<span> <FontAwesomeIcon icon={faUser} /></span>
+				<div className="account_page">
+					<div className="container mt-5">
+						<div className="d-flex justify-content-center h-100">
+							<div className="card">
+								<div className="card-header">
+									<h3>Quên mật khẩu</h3>
+									<div className="d-flex justify-content-end social_icon">
+										<span> <FontAwesomeIcon icon={faUser} /></span>
+									</div>
 								</div>
-							</div>
-							<div className="card-body">
-								<form>
-									<div className="input-group form-group">
-										<div className="input-group-prepend">
-											<span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
-										</div>
-										<input type="text" className="form-control" placeholder="Tên đăng nhập" />
+								<div className="card-body">
+									<form>
+										<div className="input-group form-group">
+											<div className="input-group-prepend">
+												<span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
+											</div>
+											<input type="text" className="form-control" placeholder="Email" />
 
-									</div>
-									<div className="form-group">
-										<input type="submit" value="Gửi" className="btn float-right login_btn" />
-									</div>
-								</form>
-							</div>
-							<div className="card-footer">
-								<div className="d-flex justify-content-center links">
-									Chưa có tài khoản? <Link to="/customer/account/register">Đăng ký</Link>
+										</div>
+										<div className="form-group">
+											<input type="submit" value="Gửi" className="btn float-right login_btn" />
+										</div>
+									</form>
 								</div>
-								<div className="d-flex justify-content-center">
-									<Link to="/customer/account/login">Đăng nhập</Link>
+								<div className="card-footer">
+									<div className="d-flex justify-content-center links">
+										Chưa có tài khoản? <Link to="/customer/account/register">Đăng ký</Link>
+									</div>
+									<div className="d-flex justify-content-center">
+										<Link to="/customer/account/login">Đăng nhập</Link>
+									</div>
 								</div>
 							</div>
 						</div>
--- src/app/code/GSM/Customer/Page/Account/Register.js
+++ src/app/code/GSM/Customer/Page/Account/Register.js
@@ -8,41 +8,49 @@ class GSMCustomerPageAccountRegister extends React.Component {
     render() {
         return (
             <>
-                <div className="container mt-5">
-                    <div className="d-flex justify-content-center h-100">
-                        <div className="card">
-                            <div className="card-header">
-                                <h3>Đăng ký</h3>
-                                <div className="d-flex justify-content-end social_icon">
-                                    <span> <FontAwesomeIcon icon={faUser} /></span>
+                <div className="account_page">
+                    <div className="container mt-5">
+                        <div className="d-flex justify-content-center h-100">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h3>Đăng ký</h3>
+                                    <div className="d-flex justify-content-end social_icon">
+                                        <span> <FontAwesomeIcon icon={faUser} /></span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="card-body">
-                                <form>
-                                    <div className="input-group form-group">
-                                        <div className="input-group-prepend">
-                                            <span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
-                                        </div>
-                                        <input type="text" className="form-control" placeholder="Tên đăng nhập" />
+                                <div className="card-body">
+                                    <form>
+                                        <div className="input-group form-group">
+                                            <div className="input-group-prepend">
+                                                <span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
+                                            </div>
+                                            <input type="text" className="form-control" placeholder="Email" />
 
-                                    </div>
-                                    <div className="input-group form-group">
-                                        <div className="input-group-prepend">
-                                            <span className="input-group-text"><FontAwesomeIcon icon={faKey} /></span>
                                         </div>
-                                        <input type="password" className="form-control" placeholder="Mật khẩu" />
-                                    </div>
-                                    <div className="form-group">
-                                        <input type="submit" value="Đăng ký" className="btn float-right login_btn" />
-                                    </div>
-                                </form>
-                            </div>
-                            <div className="card-footer">
-                                <div className="d-flex justify-content-center links">
-                                    Đã có tài khoản? <Link to="/customer/account/login">Đăng nhập</Link>
+                                        <div className="input-group form-group">
+                                            <div className="input-group-prepend">
+                                                <span className="input-group-text"><FontAwesomeIcon icon={faKey} /></span>
+                                            </div>
+                                            <input type="password" className="form-control" placeholder="Mật khẩu" />
+                                        </div>
+                                        <div className="input-group form-group">
+                                            <div className="input-group-prepend">
+                                                <span className="input-group-text"><FontAwesomeIcon icon={faKey} /></span>
+                                            </div>
+                                            <input type="password" className="form-control" placeholder="Nhập lại mật khẩu" />
+                                        </div>
+                                        <div className="form-group">
+                                            <input type="submit" value="Đăng ký" className="btn float-right login_btn" />
+                                        </div>
+                                    </form>
                                 </div>
-                                <div className="d-flex justify-content-center">
-                                    <Link to="/customer/account/forgotpassword">Quên mật khẩu?</Link>
+                                <div className="card-footer">
+                                    <div className="d-flex justify-content-center links">
+                                        Đã có tài khoản? <Link to="/customer/account/login">Đăng nhập</Link>
+                                    </div>
+                                    <div className="d-flex justify-content-center">
+                                        <Link to="/customer/account/forgotpassword">Quên mật khẩu?</Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
