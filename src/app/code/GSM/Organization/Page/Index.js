fdd7a60e212210b793b6bd9f08bc7dd76892b39a
Le Trung Kien <kienlt@smartosc.com>
2020-07-06 17:27:39 +0700

    organization

---
+++ src/app/code/GSM/Organization/Page/Index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../web/style.scss';
+
+class GSMOrganizationPageIndex extends React.Component {
+	render() {
+		return (
+			<>
+				<div className="admin_page pt-3 pb-3">
+					<div className="container">
+						<div className="row">
+							<div className="col-md-3">
+								<div className="card">
+									<div className="card-header">
+										Search & Filter
+  </div>
+									<ul className="list-group list-group-flush">
+										<li className="list-group-item">Cras justo odio</li>
+										<li className="list-group-item">Dapibus ac facilisis in</li>
+										<li className="list-group-item">Vestibulum at eros</li>
+									</ul>
+								</div>
+
+							</div>
+							<div className="col-md-9">
+								World
+						</div>
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	}
+}
+
+export default GSMOrganizationPageIndex;
