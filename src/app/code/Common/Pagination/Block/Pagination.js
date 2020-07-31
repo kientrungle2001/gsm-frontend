832a5b250c716133cc80f59c5dd56579290c2354
Le Trung Kien <kientrungle2001@gmail.com>
2020-07-31 23:08:13 +0700

    filter

--- src/app/code/Common/Pagination/Block/Pagination.js
+++ src/app/code/Common/Pagination/Block/Pagination.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import {Pagination} from '../../../GSM/Bootstrap/Block/Bootstrap';
+import {Pagination} from 'Bootstrap/Block/Bootstrap';
 
 class CommonPaginationBlockPagination extends React.Component {
+	getPageList(pages, currentPage) {
+		let pageList = [];
+		for(let i = 0; i < pages; i++) {
+			if(i === 0 || i === pages - 1 || (i <= currentPage + 4 && i >= currentPage - 4)) {
+				pageList.push(i);
+			}
+		}
+		return pageList;
+	}
 	render() {
+		let total = this.props.total;
+		let pageSize = this.props.pageSize;
+		let currentPage = this.props.currentPage || 0;
+		let pages = Math.ceil(total / pageSize);
+		let pageList = this.getPageList(pages, currentPage);
 		return (
 			<nav aria-label="Page navigation">
 				<Pagination>
-					<Pagination.Item><Pagination.Item.Link href="/">|&lt;</Pagination.Item.Link></Pagination.Item>
-					<Pagination.Item><Pagination.Item.Link href="/">&lt;&lt;</Pagination.Item.Link></Pagination.Item>
-					<Pagination.Item><Pagination.Item.Link href="/">1</Pagination.Item.Link></Pagination.Item>
-					<Pagination.Item><Pagination.Item.Link href="/">2</Pagination.Item.Link></Pagination.Item>
-					<Pagination.Item><Pagination.Item.Link href="/">3</Pagination.Item.Link></Pagination.Item>
-					<Pagination.Item><Pagination.Item.Link href="/">&gt;&gt;</Pagination.Item.Link></Pagination.Item>
-					<Pagination.Item><Pagination.Item.Link href="/">&gt;|</Pagination.Item.Link></Pagination.Item>
+					<Pagination.Item><Pagination.Item.Link href="/"  onClick={(evt) => {evt.preventDefault(); this.props.onChange(0);}}>|&lt;</Pagination.Item.Link></Pagination.Item>
+					<Pagination.Item><Pagination.Item.Link href="/" onClick={(evt) => {evt.preventDefault(); this.props.onChange(currentPage - 1 >= 0 ? currentPage - 1: 0);}}>&lt;&lt;</Pagination.Item.Link></Pagination.Item>
+					{pageList.map((page, index) => {
+						return (
+							<Pagination.Item key={index} active={page === currentPage}><Pagination.Item.Link href="/" onClick={(evt) => {
+								evt.preventDefault();
+								this.props.onChange(page);
+							}}>{page + 1}</Pagination.Item.Link></Pagination.Item>
+						);
+					})}
+					<Pagination.Item><Pagination.Item.Link href="/" onClick={(evt) => {evt.preventDefault(); this.props.onChange(currentPage + 1 < pages ? currentPage + 1: pages-1);}}>&gt;&gt;</Pagination.Item.Link></Pagination.Item>
+					<Pagination.Item><Pagination.Item.Link href="/" onClick={(evt) => {evt.preventDefault(); this.props.onChange(pages);}}>&gt;|</Pagination.Item.Link></Pagination.Item>
 				</Pagination>
 			</nav>
 		);
--- src/app/code/GSM/Organization/Page/Index.js
+++ src/app/code/GSM/Organization/Page/Index.js
@@ -1,16 +1,11 @@
-import React from 'react';
 import 'Organization/web/style.scss';
-import Pagination from 'Pagination/Block/Pagination';
-import Modal from 'Modal/Block/Modal';
-import Grid from 'Grid/Block/Grid';
 import CellModal from 'Grid/Block/Cell/Modal';
 import CellNumeric from 'Grid/Block/Cell/Numeric';
 import CellTree from 'Grid/Block/Cell/Tree';
-import TwoColumns from 'Layout/Block/TwoColumns';
-import Actions from 'Organization/Block/Index/Actions';
-import SearchForm from 'Organization/Block/Index/SearchForm';
-
-class GSMOrganizationPageIndex extends React.Component {
+import CommonGridPageGrid from 'Grid/Page/Grid';
+class GSMOrganizationPageIndex extends CommonGridPageGrid {
+	url = 'http://api2.nextnobels.com/coreusers';
+	title = 'Tổ chức';
 	getHeaders() {
 		return [
 			{
@@ -20,14 +15,24 @@ class GSMOrganizationPageIndex extends React.Component {
 				CellComponent: CellNumeric
 			},
 			{
-				name: 'Tên tổ chức',
+				name: 'Họ và tên',
 				column: 'name',
 				sortable: true,
 				CellComponent: CellTree
 			},
 			{
-				name: 'Phạm vi',
-				column: 'parent',
+				name: 'Tên đăng nhập',
+				column: 'username',
+				sortable: true,
+			},
+			{
+				name: 'Email',
+				column: 'email',
+				sortable: true,
+			},
+			{
+				name: 'Phone',
+				column: 'phone',
 				sortable: true,
 			},
 			{
@@ -41,49 +46,36 @@ class GSMOrganizationPageIndex extends React.Component {
 			},
 		];
 	}
-	getItems() {
-		return [
-			{
-				id: 1,
-				name: 'Tổng công ty',
-				parent: '',
-				type: 'Organization',
-				level: 1
-			},
-			{
-				id: 2,
-				name: 'Chi nhánh 1',
-				parent: 'Tổng công ty',
-				type: 'Branch',
-				level: 2
-			},
-			{
-				id: 3,
-				name: 'Chi nhánh 2',
-				parent: 'Tổng công ty',
-				type: 'Branch',
-				level: 2
-			}
-		];
+
+	getFilters() {
+		return [{
+			title: 'Phạm vi',
+			options: [
+				{
+					value: 1,
+					label: 'Tổng công ty'
+				},
+				{
+					value: 2,
+					label: 'Chi nhánh A'
+				},
+			]
+		},
+		{
+			title: 'Tỉnh thành',
+			options: [
+				{
+					value: 1,
+					label: 'Hà Nội'
+				},
+				{
+					value: 2,
+					label: 'Hồ Chí Minh'
+				},
+			]
+		}];
 	}
-	render() {
-		return (
-			<>
-				<Modal id="toggleModal" title="Modal Dialog Title">
-					<p>Modal content goes here!!!</p>
-				</Modal>
-				<TwoColumns rightColumnTitle="Tổ chức" leftColumnComponent={
-					<SearchForm />
-				} rightColumnComponent={
-					<>
-						<Actions />
-						<Grid items={this.getItems()} headers={this.getHeaders()} />
-						<Pagination />
-					</>
-				} />
-			</>
-		);
-	}
+
 }
 
 export default GSMOrganizationPageIndex;
