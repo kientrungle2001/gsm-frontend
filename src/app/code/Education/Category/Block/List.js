169368ce993b655475579980c93380ebfdc21ff6
Le Trung Kien <kientrungle2001@gmail.com>
2020-08-27 11:26:02 +0700

    all

--- src/app/code/Education/Category/Block/List.js
+++ src/app/code/Education/Category/Block/List.js
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+let $ = window.jQuery;
 
 export default function EducationCategoryBlockList({ currentCategoryId }) {
-	let categoryIds = [];
-	for (let i = 1; i < 11; i++) {
-		categoryIds.push(i);
-	}
+	const [categories, setCategories] = useState([]);
+	
+	useEffect(function () {
+		let real_url = 'http://w1.com/wp-json/wp/v2/course_category?_method=GET';
+		let proxy_url = 'http://w1.com/proxy.php';
+		let query = {
+			"order": "asc",
+			"orderby": "id",
+			"_fields": "id,slug,name,parent"
+		};
+		if (1) {
+			$.ajax({
+				url: proxy_url,
+				type: 'post',
+				data: {
+					method: 'post',
+					_real_url: real_url,
+					data: JSON.stringify(query)
+				},
+				dataType: 'json'
+			}).then(function (resp) {
+				setCategories(resp);
+			});
+		} else {
+			$.ajax({
+				url: real_url,
+				type: 'get',
+				data: JSON.stringify(query),
+				dataType: 'json'
+			}).then(function (resp) {
+				setCategories(resp);
+			});
+		}
+	}, []);
 	return (
 		<>
 			<ul className="list-group w-100">
-				{categoryIds.map(function (id) {
-					if(parseInt(id) === parseInt(currentCategoryId)) {
+				{categories.map(function (cat) {
+					if (parseInt(cat.id) === parseInt(currentCategoryId)) {
 						return (
-							<li className="list-group-item active" key={id}>
-								<span href={"/category/" + id}>Category {id}</span>
+							<li className="list-group-item active" key={cat.id}>
+								<span href={"/category/" + cat.id}>{cat.name}</span>
 							</li>
-						);	
+						);
 					}
 					return (
-						<li className="list-group-item" key={id}>
-							<Link to={"/category/" + id}>Category {id}</Link>
+						<li className="list-group-item" key={cat.id}>
+							<Link to={"/category/" + cat.id}>{cat.name}</Link>
 						</li>
 					);
 				})}
